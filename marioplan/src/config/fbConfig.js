2f9a02cf517c17f0855daeb523d45dbecb29864c
Brad Anderson <btanders@pm.me>
2020-07-14 20:43:59 -0700

    added firebase

---
+++ marioplan/src/config/fbConfig.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyAs7bAbxtSE-XgaRH9f2AtBMuVdKCrVwwM",
+  authDomain: "net-ninja-marioplan-936fd.firebaseapp.com",
+  databaseURL: "https://net-ninja-marioplan-936fd.firebaseio.com",
+  projectId: "net-ninja-marioplan-936fd",
+  storageBucket: "net-ninja-marioplan-936fd.appspot.com",
+  messagingSenderId: "651524328785",
+  appId: "1:651524328785:web:3fca33c9f0cf0f94575770",
+  measurementId: "G-M2STW4398Y"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase;
