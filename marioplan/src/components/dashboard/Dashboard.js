5b6821fc9bf6ce072704faa6bb200eb2f5b49552
Brad Anderson <btanders@pm.me>
2020-07-14 18:51:43 -0700

    Project details page added

--- marioplan/src/components/dashboard/Dashboard.js
+++ marioplan/src/components/dashboard/Dashboard.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Notifications from './Notifications';
 import ProjectList from '../projects/ProjectList';
 
+
 class Dashboard extends Component {
   render() {
     return (
