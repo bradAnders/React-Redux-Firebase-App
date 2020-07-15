6f65aceccc12334f1c47b3cb2c0036eb0f14af34
Brad Anderson <btanders@pm.me>
2020-07-14 17:42:32 -0700

    Added ProjectList to Dashboard

--- marioplan/src/App.js
+++ marioplan/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
+import Dashboard from './components/dashboard/Dashboard';
 
 class App extends Component {
   render () {
@@ -9,6 +10,9 @@ class App extends Component {
         <div className="App">
           <Navbar/>
         </div>
+        <Switch>
+          <Route path='/' component={ Dashboard } />
+        </Switch>
       </BrowserRouter>
     );
   }
