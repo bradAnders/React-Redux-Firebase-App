7841e13b9494359f3747f4ee1de9a8c3dc0b956d
Brad Anderson <btanders@pm.me>
2020-07-21 07:33:25 -0700

    Route guards added based on authentication

--- marioplan/src/components/projects/ProjectDetails.js
+++ marioplan/src/components/projects/ProjectDetails.js
@@ -2,9 +2,13 @@ import React from 'react'
 import { connect } from 'react-redux';
 import { firestoreConnect } from 'react-redux-firebase';
 import { compose } from 'redux';
+import { Redirect } from 'react-router-dom';
+
 
 const ProjectDetails = (props) => {
-  const { project } = props;
+  const { project, auth } = props;
+  if (!auth.uid) return <Redirect to='/signin' />
+
   return project ? (
     <div className="container section project-details">
     <div className="card z-depth-0">
@@ -31,7 +35,8 @@ const mapStateToProps = (state, ownProps) => {
   const project = projects ? projects[id] : null;
   // console.log(state);
   return {
-    project
+    project,
+    auth: state.firebase.auth
   }
 };
 
