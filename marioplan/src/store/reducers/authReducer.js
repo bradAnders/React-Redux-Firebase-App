e7e912562d049752efb6603cd7411905d768834f
Brad Anderson <btanders@pm.me>
2020-07-21 08:28:32 -0700

    remove testing comments

--- marioplan/src/store/reducers/authReducer.js
+++ marioplan/src/store/reducers/authReducer.js
@@ -6,32 +6,27 @@ const authReducer = (state=initState, action) => {
   switch (action.type) {
 
     case 'LOGIN_ERROR':
-      console.log('Login error');
       return {
         ...state,
         authError: 'Login failed'
       };
 
     case 'LOGIN_SUCCESS':
-      console.log('Login success');
       return {
         ...state,
         authError: null
       };
 
     case 'SIGNOUT_SUCCESS':
-      console.log('Signout success');
       return state
 
     case 'SIGNUP_SUCCESS':
-      console.log('Signup success');
       return {
         ...state,
         authError: null
       };
 
     case 'SIGNUP_ERROR':
-      console.log('Signup Error', action.err);
       return {
         ...state,
         authError: action.err.message
